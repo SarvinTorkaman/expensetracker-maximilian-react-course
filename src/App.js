0466ccd771af90300eef72bbe1f3db963539abca
Sarvin <sarvin.to@gmail.com>
2023-04-07 11:51:23 +0200

    organizing the component folder and adding a wrapper commmponet named Card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import ExpenseItems from "./components/ExpenseItems";
-
+import Expenses from './components/Expenses/Expenses';
 function App() {
   const expenses = [
     {
@@ -25,11 +24,8 @@ function App() {
   return (
     <div>
       <h2>Let's Start!</h2>
-      < ExpenseItems title= {expenses[0].title} amount= {expenses[0].amount} date={expenses[0].date}/>
-      < ExpenseItems title= {expenses[1].title} amount= {expenses[1].amount} date={expenses[1].date}/>
-      < ExpenseItems title= {expenses[2].title} amount= {expenses[2].amount} date={expenses[2].date}/>
-      < ExpenseItems title= {expenses[3].title} amount= {expenses[3].amount} date={expenses[3].date}/>
-    </div>
+      <Expenses expenses={expenses} />
+       </div>
   );
 }
 
--- src/components/Expenses/ExpenseItems.js
+++ src/components/Expenses/ExpenseItems.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import "./ExpenseItems.css"
-
+import ExpenseDate from "./ExpenseDate"
+import Card from "../UI/Card"
 
 
 export default function ExpenseItems(props){
-    return (<div  className='expense-item'>
 
-        <div>{props.date.toISOString()}</div>
+
+
+    return (<Card  className='expense-item'>
+
+        <ExpenseDate  date={props.date}/>
         <div className='expense-item__description'>   
              <h2 className='expense-item__description h2'> {props.title}</h2>  
              <div className='expense-item__price'>${props.amount}</div>
 
         </div>
-        </div>)
+        </Card>)
 }
